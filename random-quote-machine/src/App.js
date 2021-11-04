d3883aa4e2e1d1de07703663fdd2cdff3b25257d
Bilal <bilalahmed90@yahoo.com>
2021-11-04 22:37:38 +0000

    Add google pacifico font, icons and custom styling

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -29,14 +29,23 @@ const App = () => {
   },[])
 
   return (
-  <div className="App">
-    <div id="quote-box">
-    <div id="text">{quote}</div>
-    <div id="author">{author}</div>
-    <a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet it!</a>
-    <button id="new-quote" onClick={quoteAPI}>One more</button>
+    <div className="App">
+      <div id="quote-box" className="card border-info mb-3">
+        <div className="card-body">
+        <div id="text" className="card-text">
+          <i class="fas fa-quote-left"></i>
+          {quote}
+          <i class="fas fa-quote-right"></i>
+          </div>
+        <div id="author" className="card-title">- {author}</div>
+        </div>
+        <div className="btn btn-group">
+          <a id="tweet-quote" type="button" className="btn btn-outline-primary" href="https://twitter.com/intent/tweet"><i className="fab fa-twitter"></i></a>
+          <button id="new-quote" type="button" className="btn btn-outline-success" onClick={quoteAPI}>One more</button>
+        </div>
+      </div>
     </div>
-  </div>
-  )}
+  )
+}
 
 export default App;
