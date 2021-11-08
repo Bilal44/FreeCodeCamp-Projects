6bbd6d3ee109ca4e1dc4e3871bda7ae9012a7abd
Bilal <bilalahmed90@yahoo.com>
2021-11-08 18:39:00 +0000

    Add fcc unit tests library and properly indent code

--- javascript-calculator/src/App.js
+++ javascript-calculator/src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 const ids = {
-  7: 'seven', 
-  8: 'eight', 
-  9: 'nine', 
-  4: 'four', 
-  5: 'five', 
-  6: 'six', 
-  1: 'one', 
-  2: 'two', 
-  3: 'three', 
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  1: 'one',
+  2: 'two',
+  3: 'three',
   0: 'zero',
 }
 
 function App() {
   return (
-      <div className="calculator">              
-        <div className="nums-container">        
-          {nums.map(num => (
-            <button 
-              className={`dark-grey ${num === 0 && 'big-h'}`} 
-              key={num} 
-              id={ids[num]}
-             >
-              {num}
-            </button>
-          ))}
-        </div>
+    <div className="calculator">
+      <div className="nums-container">
+        {nums.map(num => (
+          <button
+            className={`dark-grey ${num === 0 && 'big-h'}`}
+            key={num}
+            id={ids[num]}
+          >
+            {num}
+          </button>
+        ))}
       </div>
+    </div>
   );
 }
 
