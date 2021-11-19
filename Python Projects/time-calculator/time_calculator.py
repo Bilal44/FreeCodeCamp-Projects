69fbc3845023206ed28097cc2cf448dbe0314865
Bilal <bilalahmed90@yahoo.com>
2021-11-19 15:36:43 +0000

    Implement weekdays solution and fix formatting

--- Python Projects/time-calculator/time_calculator.py
+++ Python Projects/time-calculator/time_calculator.py
@@ -1,4 +1,16 @@
-def add_time(start, duration):
+def add_time(start, duration, weekday=False):
+
+    DAYS_OF_THE_WEEK = {
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6
+    }
+
+    day, result_hours, result_day = 0, 0, ""
 
     # Getting useful data from start string
     time, am_or_pm = start.split()
@@ -15,26 +27,42 @@ def add_time(start, duration):
     duration_hours = int(duration_hours)
     duration_minutes = int(duration_minutes)
 
-    # Calculate hours and resolve next day situation
-    day = 0
-    result_hours = hours + duration_hours
-    if result_hours > 24:
-        day = result_hours / 24
-        result_hours %= 24
-    
     # Calculate minutes and resolve next hour situation
     result_minutes = minutes + duration_minutes
     if result_minutes > 60:
-        result_hours += result_minutes / 60
+        result_hours += int(result_minutes / 60)
         result_minutes %= 60
+
+    # Calculate hours and resolve next day situation
+    result_hours += hours + duration_hours
+    if result_hours > 24:
+        day = int(result_hours / 24)
+        result_hours %= 24
     
+    # 24 hour clock to AM/PM conversion
     if result_hours >= 12:
         am_or_pm = "PM"
         if result_hours >= 13:
             result_hours -= 12
     else:
         am_or_pm = "AM"
+        # Cover edge case of 00:00 hours
+        result_hours += 12 if result_hours == 0 else 0
 
-    new_time = str(result_hours).zfill(2) + ":" + str(result_minutes).zfill(2) + " " + am_or_pm
-    
+    new_time = str(result_hours) + ":" + str(result_minutes).zfill(2) + " " + am_or_pm
+
+    if weekday:
+        result_day = (DAYS_OF_THE_WEEK[weekday.lower().capitalize()] + day) % 7
+        for i, j in DAYS_OF_THE_WEEK.items():
+            if j == result_day:
+                result_day = i
+                break
+        new_time += ", " + str(result_day)
+
+    if day > 0:
+        if day == 1:
+            new_time +=  " (next day)"
+        else:
+            new_time += " (" + str(day) + " days later)"    
+
     return new_time
