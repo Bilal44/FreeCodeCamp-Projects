ed4718a72f879f8ce57d3abe1a48533bb8ee08c6
Bilal <bilalahmed90@yahoo.com>
2021-11-20 01:47:21 +0000

    Redesign to avoid repetition and clean up code

--- Python Projects/budget-app/budget.py
+++ Python Projects/budget-app/budget.py
@@ -1,43 +1,32 @@
-"""
-Complete the `Category` class in `budget.py`. It should be able to instantiate objects based on different budget categories like *food*, *clothing*, and *entertainment*. When objects are created, they are passed in the name of the category. The class should have an instance variable called `ledger` that is a list. The class should also contain the following methods:
-
-* A `deposit` method that accepts an amount and description. If no description is given, it should default to an empty string. The method should append an object to the ledger list in the form of `{"amount": amount, "description": description}`.
-* A `withdraw` method that is similar to the `deposit` method, but the amount passed in should be stored in the ledger as a negative number. If there are not enough funds, nothing should be added to the ledger. This method should return `True` if the withdrawal took place, and `False` otherwise.
-* A `get_balance` method that returns the current balance of the budget category based on the deposits and withdrawals that have occurred.
-* A `transfer` method that accepts an amount and another budget category as arguments. The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]". The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]". If there are not enough funds, nothing should be added to either ledgers. This method should return `True` if the transfer took place, and `False` otherwise.
-* A `check_funds` method that accepts an amount as an argument. It returns `False` if the amount is greater than the balance of the budget category and returns `True` otherwise. This method should be used by both the `withdraw` method and `transfer` method.
-"""
-
 class Category:
-    def __init__(self, category):
+    def __init__(self, name):
         self.ledger = []
-        self.category = category
-        self.funds = 0
+        self.name = name
     
     def deposit(self, amount, description = ""):
-        self.funds += amount
         self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description = ""):
         funds_available = self.check_funds(amount)
         if funds_available:
-            self.funds -= amount
             self.ledger.append({"amount": -amount, "description": description})
             return True
         else:
             return False
     
     def get_balance(self):
-        return self.funds
+        funds = 0
+        for item in self.ledger:
+            funds += item["amount"]
+        return funds
 
     def check_funds(self, amount):
-        return amount > self.funds
+        return self.get_balance() >= amount
 
     def transfer(self, amount, category):
         if self.check_funds(amount):
-            self.funds -= amount
-            self.withdraw(amount, "Transfer to " + category.category)
-            category.deposit(amount, "Transfer from " + self.category)
+            self.withdraw(amount, "Transfer to " + category.name)
+            category.deposit(amount, "Transfer from " + self.name)
             return True
         else:
             return False
