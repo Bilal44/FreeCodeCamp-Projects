942cc2514001545512e251d8927be7b1d8125295
Bilal <bilalahmed90@yahoo.com>
2021-11-21 15:48:35 +0000

    Generate contents list from kwargs param

--- Python Projects/probability-calculator/prob_calculator.py
+++ Python Projects/probability-calculator/prob_calculator.py
@@ -3,6 +3,12 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for k, v in kwargs.items():
+            for i in range(v):
+                self.contents.append(k)
+        print(self.contents)
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
