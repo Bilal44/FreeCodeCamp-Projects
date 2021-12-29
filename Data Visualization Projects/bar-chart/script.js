4cd561ff028e697980983b0a5832683491b75952
Bilal <bilalahmed90@yahoo.com>
2021-12-29 18:26:28 +0000

    Add x and y axis properties to bar chart

--- Data Visualization Projects/bar-chart/script.js
+++ Data Visualization Projects/bar-chart/script.js
@@ -4,5 +4,23 @@
      var margin  = {top: 100, right: 350, bottom: 50, left: 350},
          width   = 1200,
          height  = 600
- }
-  
+ 
+     
+     // X and Y axis
+     var minDate = dataset[0][0].substr(0,4);
+         minDate = new Date(minDate);
+     var maxDate = dataset[dataset.length - 1][0].substr(0,4);
+         maxDate = new Date(maxDate);
+    
+    var xAxisScale = d3.time.scale()
+        .domain([minDate, maxDate])
+        .range([0, width]);
+
+    var yAxisScale = d3.scale.linear()
+        .domain([0, d3.max(dataset, function(d) {
+            return d[1]; 
+        })
+    ])
+    .range([height, 0]);
+
+}
