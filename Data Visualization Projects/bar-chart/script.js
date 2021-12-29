acb01b45a801a296e779e1a6272cd7508b383121
Bilal <bilalahmed90@yahoo.com>
2021-12-29 17:43:18 +0000

    Improve layout, define d3 chart area

--- Data Visualization Projects/bar-chart/script.js
+++ Data Visualization Projects/bar-chart/script.js
@@ -1,30 +1,8 @@
- // javascript
- var dataset = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
- console.log(data)
 
- var svgWidth = 500; 
- var svgHeight = 300
- var barPadding = 5;
- var barWidth = (svgWidth / dataset.length);
- 
- 
- var svg = d3.select('svg')
-     .attr("width", svgWidth)
-     .attr("height", svgHeight);
-
- var barChart = svg.selectAll("rect")
-     .data(dataset)
-     .enter()
-     .append("rect")
-     .attr("y", function(d) {
-          return svgHeight - d 
-     })
-     .attr("height", function(d) { 
-         return d; 
-     })
-     .attr("width", barWidth - barPadding)
-     .attr("transform", function (d, i) {
-         var translate = [barWidth * i, 0]; 
-         return "translate("+ translate +")";
-     });
- 
+ function PopulateChart(dataset){
+    // Chart area
+     var margin  = {top: 100, right: 350, bottom: 50, left: 350},
+         width   = 1200,
+         height  = 600
+ }
+  
