d81d290741ff24141624a978f6a9adda1a18eebe
Bilal <bilalahmed90@yahoo.com>
2022-01-01 17:37:56 +0000

    Add remote json file and function calls to populate a scatter graph

---
+++ Data Visualization Projects/scatterplot-graph/script.js
@@ -0,0 +1,9 @@
+function populateScatterGraph(url){
+    
+  }
+
+var draw = () => {
+    populateScatterGraph("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json");
+}
+
+draw.call();
