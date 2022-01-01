a3e31ef8441461c3a27e1f9afc0959dd3c1589ad
Bilal <bilalahmed90@yahoo.com>
2022-01-01 19:08:32 +0000

    Define chart area with x and y axis scaling/labels

--- Data Visualization Projects/scatterplot-graph/script.js
+++ Data Visualization Projects/scatterplot-graph/script.js
@@ -1,6 +1,22 @@
 function populateScatterGraph(url){
+    // Chart area
+    var margin = { top: 60, left: 60, bottom: 60, right: 90 } 
+    var height = 480, width = 780;
     
-  }
+    // Time parser for race times
+    var timeParser = d3.timeParse("%M:%S");
+    
+    // X and Y axis scaling
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleTime().range([0, height]);
+    
+    // X and Y axis labels
+    var xAxis = d3.axisBottom(x)
+        .tickFormat(d3.format("d"));
+
+    var yAxis = d3.axisLeft(y)
+        .tickFormat(d3.timeFormat("%M:%S"));
+}
 
 var draw = () => {
     populateScatterGraph("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json");
