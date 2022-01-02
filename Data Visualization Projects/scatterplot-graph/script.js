b05168b3e9089362b117a389ab762f7bc1cb9b95
Bilal <bilalahmed90@yahoo.com>
2022-01-02 13:22:31 +0000

    Populate the graph from json data file

--- Data Visualization Projects/scatterplot-graph/script.js
+++ Data Visualization Projects/scatterplot-graph/script.js
@@ -33,6 +33,58 @@ function populateScatterGraph(url){
 
     svg = svg.append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");    
+        
+    // Populate the scatterplot graph from remote json data file
+    d3.json(url)
+        .then((data) => {
+            var fastest = d3.min(data.map((item) => { return timeParser(item.Time); }));
+            var slowest = d3.max(data.map((item) => { return timeParser(item.Time); }));
+            
+             x.domain([d3.min(data, (d) => { return d.Year; }), d3.max(data, (d) => { return d.Year; })]);
+             y.domain([slowest, fastest]);
+    
+             svg.append("g")
+                 .attr("id", "x-axis")
+                 .attr("transform", "translate(0," + height + ")")
+                 .call(xAxis)
+               .append("text")
+                 .attr("transform", "translate(" + width + ",-30)")
+                 .attr("dy", "1.8em")
+                 .attr("text-anchor", "end")
+                 .text("Race time for 13.8km");
+            
+             svg.append("g")
+                 .attr("id", "y-axis")
+                 .call(yAxis)
+               .append("text")
+                 .attr("transform", "rotate(-90)")
+                 .attr("dy", "-0.8em")
+                 .attr("text-anchor", "end")
+                 .text("Position");
+            
+            // Add inidividual data entities (cyclist) as a circle on the chart
+             var cyclist = svg.selectAll(".cyclist")
+                 .data(data)
+               .enter().append("g")
+                 .attr("class", "cyclist")
+                 .attr("x", (d) => { return x(d.Year); })
+                 .attr("y", (d) => { return y(timeParser(d.Time)); })
+            
+             cyclist.append("circle")
+                 .attr("class", "dot")
+                 .attr("cx", (d) => { return x(d.Year); })
+                 .attr("cy", (d) => { return y(timeParser(d.Time)); })
+                 .attr("data-xvalue", (d) => { return d.Year; })
+                 .attr("data-yvalue", (d) => { return timeParser(d.Time); })
+                 .attr("r", 5)
+                 .attr("fill", (d) => { return d.Doping; })
+            
+             // Add the cyclist's name next to their circle on the chart
+             cyclist.append("text")
+                 .attr("x", (d) => { return x(d.Year) + 5; })
+                 .attr("y", (d) => { return y(timeParser(d.Time)) + 7; })
+                 .text((d) => { return d.Name; }); 
+    });
 }
 
 var draw = () => {
