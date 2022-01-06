3a05e091a9373b527a62e2d0a11eb694a9c26a6b
Bilal <bilalahmed90@yahoo.com>
2022-01-06 18:17:42 +0000

    Add chart area and canvas for heat map

---
+++ Data Visualization Projects/heat-map/script.js
@@ -0,0 +1,18 @@
+// Chart area
+var height = 500, width = 1000,
+    margin = {
+      left: 120, right: 20, top: 20, bottom: 70
+    },
+    url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+    colour = ["#ef5350","#EC407A","#AB47BC","#7E57C2","#5C6BC0","#42A5F5","#26C6DA","#26A69A","#D4E157","#FFEE58","#FFA726"];
+
+// Heat map canvas
+var canvas = d3.select('svg').attr({
+  height: height + margin.top + margin.bottom,
+  width: width + margin.left + margin.right
+});
+
+var group = canvas.append('g').attr({
+  transform: "translate(" + margin.left + "," + margin.top + ")"
+});
