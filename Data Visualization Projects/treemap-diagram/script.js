88bd1c15200cdb08c512212a4a3e4f0e7a3259d9
Bilal <bilalahmed90@yahoo.com>
2022-01-13 20:07:46 +0000

    Set up treemap and add colour schemes

--- Data Visualization Projects/treemap-diagram/script.js
+++ Data Visualization Projects/treemap-diagram/script.js
@@ -1,6 +1,25 @@
 async function start() {
   const jsonURL = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json');
-  const jsonData = await jsonURL.json();  
+  const jsonData = await jsonURL.json();
+
+  const width = 960;
+  const height = 600;
+  
+  const colour = d3.scaleOrdinal(d3.schemeCategory10);
+  
+  const svg = d3.select('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('margin-left', 425);
+  
+  const treemap = d3.treemap()
+    .size([width, height])
+    .padding(0.5)
+
+  const root = d3.hierarchy(jsonData)
+    .sum(d => d.value)
+  
+  treemap(root);
 }
 
 start();
