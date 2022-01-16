79dacff3db43408407c4b6a6325cb9284fb4762c
Bilal <bilalahmed90@yahoo.com>
2022-01-16 19:41:10 +0000

    Add convert API endpoint with appropriate responses

--- Quality Assurance Projects/metric-imperial-converter/routes/api.js
+++ Quality Assurance Projects/metric-imperial-converter/routes/api.js
@@ -7,4 +7,17 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+  
+    if(!initNum && !initUnit) {
+      res.send("invalid number and unit");
+    } else if (!initNum) {
+      res.send("invald number");
+    } else {
+      res.send("invalid unit");
+    }
+  })
 };
