16c20503afbbb10e87d3f881c16d916199e941eb
Bilal <bilalahmed90@yahoo.com>
2022-01-19 19:53:03 +0000

    Add unit test to correctly read a whole number input

--- Quality Assurance Projects/metric-imperial-converter/tests/1_unit-tests.js
+++ Quality Assurance Projects/metric-imperial-converter/tests/1_unit-tests.js
@@ -4,6 +4,13 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite('Unit Tests', function () {
+  suite("Function convertHandler.getNum(input)", function () {
+    
+    // Correctly read a whole number input
+    test("Whole Number Input Test", function () {
+      let input = "4L";
+      assert.equal(convertHandler.getNum(input), 4);
+    });
+  });
 });
