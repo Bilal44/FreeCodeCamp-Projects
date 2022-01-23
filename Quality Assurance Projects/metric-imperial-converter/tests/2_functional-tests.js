4747387678e8d688249c68d6436bf81f9adbad0b
Bilal <bilalahmed90@yahoo.com>
2022-01-23 00:40:20 +0000

    Add /api/convert functional test - invalid number and unit

--- Quality Assurance Projects/metric-imperial-converter/tests/2_functional-tests.js
+++ Quality Assurance Projects/metric-imperial-converter/tests/2_functional-tests.js
@@ -46,6 +46,19 @@ suite('Functional Tests', function () {
                         assert.equal(res.body.initNum, undefined);
                     });
             });
+
+            // Convert an invalid number AND unit such as `3/7.2/4kilomegagram`: `GET` request to `/api/convert`
+            test("3/7.2/4kilomegagram (Invalid Number and Unit) Conversion Request Test", function () {
+                chai
+                    .request(server)
+                    .get("/api/convert")
+                    .query({ input: "3/7.2/4kilomegagram" })
+                    .end(function (err, res) {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, undefined);
+                        assert.equal(res.body.initUnit, undefined);
+                    });
+            });
         });
     });
 });
