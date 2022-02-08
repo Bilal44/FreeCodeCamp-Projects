782133c63b6810d92c0ff721c9d39276196811f1
Bilal <bilalahmed90@yahoo.com>
2022-02-08 15:01:48 +0000

    Complete unit test 1 - valid puzzle input

--- Quality Assurance Projects/sudoku-solver/tests/1_unit-tests.js
+++ Quality Assurance Projects/sudoku-solver/tests/1_unit-tests.js
@@ -2,8 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
+let validPuzzleInput = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
 suite('UnitTests', () => {
-
+    suite("solver tests", function () {
+        test("1 - Logic handles a valid puzzle string of 81 characters", function () {
+            let completedPuzzle = "769235418851496372432178956174569283395842761628713549283657194516924837947381625";
+            assert.equal(solver.solve(validPuzzleInput), completedPuzzle);
+        });
+    });
 });
