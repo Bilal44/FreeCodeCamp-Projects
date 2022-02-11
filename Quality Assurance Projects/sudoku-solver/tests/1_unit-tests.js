524142a0f291510fb82aca2c9ffaae56cf312fc3
Bilal <bilalahmed90@yahoo.com>
2022-02-11 21:21:10 +0000

    Complete functional test 1 - solve a puzzle with valid puzzle string

--- Quality Assurance Projects/sudoku-solver/tests/1_unit-tests.js
+++ Quality Assurance Projects/sudoku-solver/tests/1_unit-tests.js
@@ -6,7 +6,7 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver = new Solver();
 let validPuzzleInput = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
-suite('UnitTests', () => {
+suite('Unit Tests', () => {
     suite('1 - Valiation Tests', () => {
         test("1.1 - Logic handles a valid puzzle string of 81 characters", function () {
             assert.isTrue(solver.validate(validPuzzleInput));
--- Quality Assurance Projects/sudoku-solver/tests/2_functional-tests.js
+++ Quality Assurance Projects/sudoku-solver/tests/2_functional-tests.js
@@ -5,7 +5,20 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+let validPuzzleInput = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+
 suite('Functional Tests', () => {
-
+    suite('/api/solve End Point Tests', () => {
+        test("1 - Solve a puzzle with valid puzzle string: POST request to /api/solve", function (done) {
+            chai.request(server)
+                .post("/api/solve")
+                .send({ puzzle: validPuzzleInput })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    let solvedPuzzle = "769235418851496372432178956174569283395842761628713549283657194516924837947381625";
+                    assert.equal(res.body.solution, solvedPuzzle);
+                    done();
+                });
+        });
+    });
 });
-
