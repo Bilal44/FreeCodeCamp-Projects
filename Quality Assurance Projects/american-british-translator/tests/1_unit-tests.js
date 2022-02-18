197fb586af17c51a0819ee537936f1827b5ef2f2
Bilal <bilalahmed90@yahoo.com>
2022-02-18 21:18:04 +0000

    Complete British to American translation unit tests

--- Quality Assurance Projects/american-british-translator/tests/1_unit-tests.js
+++ Quality Assurance Projects/american-british-translator/tests/1_unit-tests.js
@@ -74,4 +74,67 @@ suite('Unit Tests', () => {
             );
         });
     });
+
+    suite("2 - American English Translation Tests", function () {
+        test("2.1 - Translate `We watched the footie match for a while.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("We watched the footie match for a while.")[0],
+                "We watched the soccer match for a while."
+            );
+        });
+
+        test("2.2 - Translate `Paracetamol takes up to an hour to work.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("Paracetamol takes up to an hour to work.")[0],
+                "Tylenol takes up to an hour to work."
+            );
+        });
+
+        test("2.3 - Translate `First, caramelise the onions.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("First, caramelise the onions.")[0],
+                "First, caramelize the onions."
+            );
+        });
+
+        test("2.4 - Translate `I spent the bank holiday at the funfair.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("I spent the bank holiday at the funfair.")[0],
+                "I spent the public holiday at the carnival."
+            );
+        });
+
+        test("2.5 - Translate `I had a bicky then went to the chippy.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("I had a bicky then went to the chippy.")[0],
+                "I had a cookie then went to the fish-and-chip shop."
+            );
+        });
+
+        test("2.6 - Translate `I've just got bits and bobs in my bum bag.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("I've just got bits and bobs in my bum bag.")[0],
+                "I've just got odds and ends in my fanny pack."
+            );
+        });
+
+        test("2.7 - Translate `The car boot sale at Boxted Airfield was called off.` to American English", function () {
+            assert.equal(
+                translator.toAmericanEnglish("The car boot sale at Boxted Airfield was called off.")[0],
+                "The swap meet at Boxted Airfield was called off."
+            );
+        });
+
+        test("2.8 - Translate `Have you met Mrs Kalyani?` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("Have you met Mrs Kalyani?")[0],
+                "Have you met Mrs. Kalyani?"
+            );
+        });
+
+        test("2.9 - Translate `Prof Joyner of King's College, London.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("Prof Joyner of King's College, London.")[0],
+                "Prof. Joyner of King's College, London."
+            );
+        });
+
+        test("2.10 - Translate `Tea time is usually around 4 or 4.30.` to American English", function () {
+            assert.equal(translator.toAmericanEnglish("Tea time is usually around 4 or 4.30.")[0],
+                "Tea time is usually around 4 or 4:30."
+            );
+        });
+    });
 });
