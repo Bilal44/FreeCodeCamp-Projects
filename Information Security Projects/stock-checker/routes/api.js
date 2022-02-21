8a97f20218e8171a3326cbb5a14dee4544100320
Bilal <bilalahmed90@yahoo.com>
2022-02-21 19:07:34 +0000

    Get share name and price from FCC API with fetch

--- Information Security Projects/stock-checker/routes/api.js
+++ Information Security Projects/stock-checker/routes/api.js
@@ -1,4 +1,5 @@
 'use strict';
+const fetch = require("node-fetch");
 
 module.exports = function (app) {
 
@@ -8,3 +9,11 @@ module.exports = function (app) {
     });
     
 };
+
+async function getStock(stock) {
+  const response = await fetch(
+    'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote'
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
