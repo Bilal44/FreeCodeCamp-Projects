b99e6a8aeaf427d71e430af67320ea8cb46ceecf
Bilal <bilalahmed90@yahoo.com>
2022-02-22 21:17:57 +0000

    Add function test 1.1 - view one stock

--- Information Security Projects/stock-checker/tests/2_functional-tests.js
+++ Information Security Projects/stock-checker/tests/2_functional-tests.js
@@ -5,6 +5,20 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite('Functional Tests', function () {
+    suite("1 - /api/stock-prices/ GET Request Tests", function () {
+        test("1.1 - Viewing one stock: GET request to `/api/stock-prices/`", function (done) {
+            chai
+                .request(server)
+                .get("/api/stock-prices/")
+                .set("content-type", "application/json")
+                .query({ stock: "MSFT" })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData.stock, "MSFT");
+                    assert.exists(res.body.stockData.price);
+                    done();
+                });
+        });
+    });
 });
