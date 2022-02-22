3e00824421bd50e1add6ab3126c995cb50824ce2
Bilal <bilalahmed90@yahoo.com>
2022-02-22 21:49:06 +0000

    Add functional test 1.3 - view and like the same stock again

--- Information Security Projects/stock-checker/tests/2_functional-tests.js
+++ Information Security Projects/stock-checker/tests/2_functional-tests.js
@@ -35,5 +35,20 @@ suite('Functional Tests', function () {
                     done();
                 });
         });
+        
+        test("1.3 - Viewing the same stock and liking it again: GET request to `/api/stock-prices/`", function (done) {
+            chai
+                .request(server)
+                .get("/api/stock-prices/")
+                .set("content-type", "application/json")
+                .query({ stock: "BP", like: true })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData.stock, "BP");
+                    assert.equal(res.body.stockData.likes, 1);
+                    assert.exists(res.body.stockData.price);
+                    done();
+                });
+        });
     });
 });
