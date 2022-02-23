dd3a595b80c637c22a22635342b004a2ea0a87b0
Bilal <bilalahmed90@yahoo.com>
2022-02-23 20:39:41 +0000

    Fix order of schemas in models.js

--- Information Security Projects/anonymous-message-board/models.js
+++ Information Security Projects/anonymous-message-board/models.js
@@ -1,17 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const ThreadSchema = new Schema({
-    text: { type: String, required: true },
-    delete_password: { type: String },
-    board: { type: String, required: true },
-    created_on: { type: Date, required: true, default: new Date() },
-    bumped_on: { type: Date, default: new Date() },
-    reported: { type: Boolean },
-    replies: [replySchema]
-})
-let Thread = mongoose.model('Thread', ThreadSchema)
-
 const ReplySchema = new Schema({
     text: { type: String, required: true },
     delete_password: { type: String },
@@ -19,6 +8,17 @@ const ReplySchema = new Schema({
     reported: { type: Boolean }
 })
 let Reply = mongoose.model('Reply', ReplySchema)
+
+const ThreadSchema = new Schema({
+    text: { type: String, required: true },
+    delete_password: { type: String },
+    board: { type: String, required: true },
+    created_on: { type: Date, required: true, default: new Date() },
+    bumped_on: { type: Date, default: new Date() },
+    reported: { type: Boolean },
+    replies: [ReplySchema]
+})
+let Thread = mongoose.model('Thread', ThreadSchema)
 
 exports.Thread = Thread;
 exports.Reply = Reply;
