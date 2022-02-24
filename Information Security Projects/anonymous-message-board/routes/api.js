08e39fa881210c916c9c1fda1bf9141b94fa1ca3
Bilal <bilalahmed90@yahoo.com>
2022-02-24 22:57:29 +0000

    Add /api/threads/:board GET request

--- Information Security Projects/anonymous-message-board/routes/api.js
+++ Information Security Projects/anonymous-message-board/routes/api.js
@@ -1,8 +1,15 @@
 'use strict';
+const ThreadModel = require("../models").Thread;
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
+
+  app.route('/api/threads/:board')
+    .get((req, res) => {
+      ThreadModel.find({ board: req.params.board })
+        .exec((err, threads) => {
+          res.json(threads);
+        });
+    })
     
   app.route('/api/replies/:board');
 
