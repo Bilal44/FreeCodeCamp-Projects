3469ace2540789b4949568bc81cd2ce3edb2fbc2
Bilal <bilalahmed90@yahoo.com>
2022-02-24 23:27:34 +0000

    Add /api/threads/:board POST request

--- Information Security Projects/anonymous-message-board/routes/api.js
+++ Information Security Projects/anonymous-message-board/routes/api.js
@@ -3,6 +3,7 @@ const ThreadModel = require("../models").Thread;
 
 module.exports = function (app) {
 
+  // Thread API end points
   app.route('/api/threads/:board')
     .get((req, res) => {
       ThreadModel.find({ board: req.params.board })
@@ -10,7 +11,30 @@ module.exports = function (app) {
           res.json(threads);
         });
     })
-    
+
+    .post((req, res) => {
+      const { board, text, delete_password } = req.body;
+
+      if (!board) {
+        board = req.params.board;
+      }
+
+      const thread = new ThreadModel({
+        text: text,
+        delete_password: delete_password,
+        board,
+        replies: [],
+      });
+
+      thread.save((err, data) => {
+        if (err || !data) {
+          res.send("An error occured while saving the post.");
+        } else {
+          res.json(thread);
+        }
+      });
+    })
+
   app.route('/api/replies/:board');
 
 };
