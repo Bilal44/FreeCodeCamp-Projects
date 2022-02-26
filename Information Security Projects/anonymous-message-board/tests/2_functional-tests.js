e4994cbab511048ceb932b53a3d7bc0366959bae
Bilal <bilalahmed90@yahoo.com>
2022-02-26 21:43:34 +0000

    Complete `/api/threads/{board}` functional tests

--- Information Security Projects/anonymous-message-board/tests/2_functional-tests.js
+++ Information Security Projects/anonymous-message-board/tests/2_functional-tests.js
@@ -5,6 +5,65 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+let threadID;
 
+suite('Functional Tests', function () {
+
+    // Functional tests to test `/api/threads/{board}` RESTful end points
+    suite("1 - `/api/threads/{board}` Functional Tests", function () {
+        test("1.1 - Creating a new thread: POST Request", function (done) {
+            chai
+                .request(server)
+                .post("/api/threads/test-board")
+                .set("content-type", "application/json")
+                .send({ text: "test thread", delete_password: "pass" })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.text, "test thread");
+                    assert.equal(res.body.delete_password, "pass");
+                    assert.equal(new Date(res.body.created_on).toDateString(), new Date().toDateString());
+                    assert.isArray(res.body.replies);
+                    threadID = res.body._id;
+                    done();
+                });
+        });
+
+        test("1.2 - Viewing the 10 most recent threads with 3 replies each: GET Request Test", function (done) {
+            chai
+                .request(server)
+                .get("/api/threads/test-board")
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.exists(res.body[0]);
+                    assert.equal(res.body[0].text, "test thread");
+                    done();
+                });
+        });
+
+        test("1.3 - Deleting a thread with the incorrect password: DELETE request Test", function (done) {
+            chai
+                .request(server)
+                .delete("/api/threads/test-board")
+                .set("content-type", "application/json")
+                .send({ thread_id: threadID, delete_password: "incorrect" })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, "incorrect password");
+                    done();
+                });
+        });
+
+        test("1.4 - Reporting a thread: PUT Request Test", function (done) {
+            chai
+                .request(server)
+                .put("/api/threads/test-board")
+                .set("content-type", "application/json")
+                .send({ thread_id: threadID })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, "reported");
+                    done();
+                });
+        });
+    });
 });
