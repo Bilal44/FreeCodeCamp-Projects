5616b847e4b007556c78672364a2dba31afe2ad2
Bilal <bilalahmed90@yahoo.com>
2022-02-27 17:45:38 +0000

    Capitalise the start of the statments to match formatting

--- Python Projects/budget-app/main.py
+++ Python Projects/budget-app/main.py
@@ -4,16 +4,16 @@ from budget import create_spend_chart
 from unittest import main
 
 food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
+food.deposit(1000, "Initial deposit")
+food.withdraw(10.15, "Groceries")
+food.withdraw(15.89, "Restaurant and more food for dessert")
 print(food.get_balance())
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
 clothing.withdraw(25.55)
 clothing.withdraw(100)
 auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
+auto.deposit(1000, "Initial deposit")
 auto.withdraw(15)
 
 print(food)
