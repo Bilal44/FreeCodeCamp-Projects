63376488a6fd6002dddcff0c48dda0fa4e09d9cb
Bilal <bilalahmed90@yahoo.com>
2022-02-28 19:39:07 +0000

    Use socket library to test remote target ports

--- Information Security Projects/port-scanner/port_scanner.py
+++ Information Security Projects/port-scanner/port_scanner.py
@@ -3,6 +3,20 @@ import socket
 def get_open_ports(target, port_range):
     open_ports = []
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    
+    targetIP = socket.gethostbyname(target)
 
+    for port in port_range:
+      try:
+        result = sock.connect_ex((targetIP, port))
+        if result == 0: 
+          open_ports.append(port)
+      except Exception as ex:
+        print(ex)
+        continue
+
+    sock.close()
 
     return(open_ports)
