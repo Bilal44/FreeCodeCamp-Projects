8d2985248adabd6a2ce198ec3357b0363ef2e42a
Bilal <bilalahmed90@yahoo.com>
2022-02-28 21:09:39 +0000

    Add and return verbose response

--- Information Security Projects/port-scanner/port_scanner.py
+++ Information Security Projects/port-scanner/port_scanner.py
@@ -1,7 +1,8 @@
 import socket
 import re
+from common_ports import ports_and_services
 
-def get_open_ports(target, port_range):
+def get_open_ports(target, port_range, verbose=False):
     open_ports = []
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,5 +29,13 @@ def get_open_ports(target, port_range):
         print(ex)
         continue
       sock.close()
+        
+    response = open_ports
 
-    return(open_ports)
+    if (verbose):
+      response = "Open ports for " + target
+      for port in open_ports:
+        service = ports_and_services.get(port)
+        response += "\nPORT     SERVICE\n"+ str(port)+ "      " + service
+
+    return(response)
