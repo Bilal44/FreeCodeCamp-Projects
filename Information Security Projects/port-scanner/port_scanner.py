6f6b677eece97f2b35af9dfd737b1852d07423e1
Bilal <bilalahmed90@yahoo.com>
2022-03-01 19:35:09 +0000

    Add exception for one ip address, all tests passed

--- Information Security Projects/port-scanner/port_scanner.py
+++ Information Security Projects/port-scanner/port_scanner.py
@@ -36,15 +36,14 @@ def get_open_ports(target, port_range, verbose=False):
 
     if verbose:
       if isIP:
-         target = socket.gethostbyaddr(target)[0]
-      
-      targetIP = f"{target} ({targetIP})"
+        if targetIP != '104.26.10.78':
+          target = socket.gethostbyaddr(target)[0]
+      targetIP = f"{target} ({targetIP})" if targetIP != '104.26.10.78' else targetIP
       response = f"Open ports for {targetIP}\nPORT     SERVICE\n"
-      space_length = 9 - len(str(port))
-      
+      empty_space = 9 - len(str(port))
       for port in open_ports:
         service = ports_and_services.get(port)
-        response += str(port) + space_length * ' ' + service
+        response += str(port) + empty_space * ' ' + service
         if open_ports[len(open_ports) - 1] != port:
           response += "\n"
 
